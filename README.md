d7173588b9ba601ee6d6e9bc31c1f27c5067c120
Edisson Reinozo <edisson350@gmail.com>
2018-03-06 16:06:04 -0500

    Basic implementation completed -- Test Needed

--- README.md
+++ README.md
@@ -1,18 +1,30 @@
 # ucuenca.js
-Librería de Python para la API de la Universidad de Cuenca - Port of https://github.com/stsewd/ucuenca.py
+Librería de Python para la API de la Universidad de Cuenca
+
+## UNDER DEVELOPMENT!!
+
+## Install
+
+`npm install --save ucuenca`
+
 
 ## Descripción
 
 Librería de JavaScript para la API de la [Universidad de Cuenca](http://www.ucuenca.edu.ec/). Basada en [ucuenca.py](https://github.com/stsewd/ucuenca.py)
 
 
-```python
-from ucuenca import Ucuenca
-
-const uc = Ucuenca()
+```javascript
+const Ucuenca = require('ucuenca');
+const uc = new Ucuenca();
 # Un estudiante puede cursar varias carreras,
 # obtenemos una sola.
-const careers = uc.careers('0104926787')[0]
-careers['malla']  # MALLA CONTABILIDAD Y AUDITORIA 2008
-careers['fecha_matricula']  # 2013-03-03 09:37:08.0
+uc.careers('0104926787', (err, res) => {
+  if (err) { 
+    console.log(err);
+    return;
+  };
+  const career = res[0];
+  console.log(career['MALLA']); # MALLA CONTABILIDAD Y AUDITORIA 2008
+  console.log(career['FECHA_MATRICULA']); # 2013-03-03 09:37:08.0
+});
 ```
--- index.js
+++ index.js
@@ -4,66 +4,62 @@ const http = require('http');
 const BASE_URL = "http://evaluacion.ucuenca.edu.ec/ucuenca-rest-ws/api/v1/"
 
 class Ucuenca {
-  careers(student_id) {
-    const response = this.get('registroacademico', {'idEstudiante': student_id});
-    return response;     
+  careers(student_id, callback) {
+    const response = this.get('registroacademico', {'idEstudiante': student_id}, callback);
   }
 
-  notes(student_id, career_id, period_id){
+
+  notes(student_id, career_id, period_id, callback){
     const params = {
-      'idEstudiante': student_id,
-      'idCarrera': career_id,
-      'idPerlec': period_id
+      idEstudiante: student_id,
+      idCarrera: career_id,
+      idPerlec: period_id
     }
 
-    return this.get('registroacademico/notas', params);
+    return this.get('registroacademico/notas', params, callback);
   }
 
-  schedule(student_id){
+  schedule(student_id, career_id=1, curriculum_id, career_plan, callback){
     const params={
-      'idEstudiante': student_id,
-      'idCarrera': career_id,
-      'idMalla': curriculum_id,
-      'idPlacar': career_plan
+      idEstudiante: student_id,
+      idCarrera: career_id,
+      idMalla: curriculum_id,
+      idPlacar: career_plan
     }
-    return this.get('registroacademico/malla', params);
+    return this.get('registroacademico/malla', params, callback);
   }
 
-  authentication(user, passw){
+  authentication(user, passw, callback){
     const params={
-      'usuario': user,
-      'pass': passw
+      usuario: user,
+      pass: passw
     }
-    return this.get('acceso', params);
+    return this.get('acceso', params, callback);
   }
 
-  get(service_name, params){
+  get(service_name, params, callback){
     const url = this.get_url(service_name, params);
-    const response = this.request(url);
-    return response;
+    console.log(url);
+    const response = this.request(url, callback);
   }
 
   get_url(service_name, params){
     return `${BASE_URL}${service_name}?${querystring.stringify(params)}`;
   }
 
-  request(url){
-    let data = '1';
+  request(url, callback){
+    callback = callback || function(){};
+
     http.get(url, (res) => {
-      const { statusCode } = res;
-      
+      const { statusCode } = res;     
       let error;
       if(statusCode !== 200){
         error = new Error('Request Failed.\n' + 
                           `Status Code: ${statusCode}`);
-      }
-
-      if (error) {
-        console.error(error.message);
+        callback(error, null)         
         res.resume();
-        return;
+        return;                         
       }
-
       res.setEncoding('utf8');
       let rawData = '';
       res.on('data', (chuck) => { rawData += chuck; });
@@ -71,20 +67,16 @@ class Ucuenca {
       res.on('end', () => { 
         try {
           const parsedData = JSON.parse(rawData);
-          console.log(parsedData);
+          callback(error, parsedData);
+
         } catch (e) {
-          console.error(e.message);
+          callback(e, null);
         }
       }).on('error', (e) => {
-        console.error(e.message);
+        callback(e, null);
       });
-      
-
     });
-    return data;
   } 
 }
 
-uc = new Ucuenca();
-
-console.log(uc.careers(1400757116));
+module.exports = Ucuenca;
